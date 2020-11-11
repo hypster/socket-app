fc288603d287669e410f4c94b6cb6b87d422925e
Michal Jarski <michal@jarski.eu>
2020-11-11 07:38:46 +0100

    Add second client

---
+++ client2.py
@@ -0,0 +1,20 @@
+import socket
+import json
+
+with open("json_files/config_2.json") as json_file:
+    json_data = json.load(json_file)
+    HOST = json_data['server']['ip']
+    PORT = int(json_data['server']['port'])
+
+public_json_data = json.dumps([{'id': json_data['person']['id'], 'name': json_data['person']['name'],
+                                'public_key': json_data['person']['keys']['public']}])
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(bytes(public_json_data, encoding='UTF-8'))
+    while True:
+        data = s.recv(1024)
+        print('Received', repr(data))
+        text = input()
+        s.sendall(bytes(text, encoding='UTF-8'))
+
